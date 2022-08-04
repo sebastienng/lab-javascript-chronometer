95c982159f3c7506e16ca1aba2c857bc6219092a
Sebastien Niang <sebastien.niang@gmail.com>
2022-08-04 10:09:37 +0200

    need to refactor but working

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,74 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime=0;
+    this.centiseconds = 0
+    this.intervalId=null;
   }
 
   start(callback) {
     // ... your code goes here
+    this.invervalId= setInterval(()=>{
+      this.centiseconds++;
+      if(this.centiseconds===100) {
+        this.centiseconds=0;
+        this.currentTime++;
+      }
+      
+      if(callback){
+        callback();
+      }
+      console.log(this.currentTime);
+    },10)
+   
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
+
 
   getSeconds() {
     // ... your code goes here
+  
+      return this.currentTime%60;
+  
+  }
+  getMilli() {
+    // ... your code goes here
+  
+      return this.centiseconds;
+  
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if(value<10){
+      return `0${value}`;
+    }
+    return `${value}`;
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.invervalId);
+
+    this.intervalId=null;
+
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime=0;
+    this.centiseconds=0;
   }
 
   split() {
     // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const second = this.computeTwoDigitNumber(this.getSeconds());
+    const milli = this.computeTwoDigitNumber(this.getMilli());
+    return `${minutes}:${second}:${milli}`;
   }
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -15,19 +15,30 @@ const splitsElement = document.getElementById('splits');
 
 function printTime() {
   // ... your code goes here
+  minDecElement.innerHTML=`${this.printMinutes()[0]}`;
+  minUniElement.innerHTML=`${this.printMinutes()[1]}`;
+  secDecElement.innerHTML=`${this.printSeconds()[0]}`;
+  secUniElement.innerHTML=`${this.printSeconds()[1]}`;
+  milDecElement.innerHTML=`${this.printMilliseconds()[0]}`;
+  milUniElement.innerHTML=`${this.printMilliseconds()[1]}`;
 }
 
 function printMinutes() {
   // ... your code goes here
+
+  return chronometer.computeTwoDigitNumber(chronometer.getMinutes());
 }
 
 function printSeconds() {
   // ... your code goes here
+  return chronometer.computeTwoDigitNumber(chronometer.getSeconds());
 }
 
 // ==> BONUS
 function printMilliseconds() {
   // ... your code goes here
+ 
+  return chronometer.computeTwoDigitNumber(chronometer.getMilli());
 }
 
 function printSplit() {
@@ -57,9 +68,53 @@ function setResetBtn() {
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
   // ... your code goes here
+
+  /*Once the btn is clicked i check if the start class was in the btn
+  class list. If not, I start the the chrono and change the text.
+  Else i stop the chrono and change the txt
+
+  */
+  if(btnLeftElement.classList.toggle('start')){
+    
+    btnRightElement.innerHTML='RESET';
+    btnLeftElement.innerHTML='START';
+    
+    
+    chronometer.stop();
+    
+  }else{
+    chronometer.start(printTime);
+    btnLeftElement.innerHTML='STOP';
+    btnRightElement.innerHTML='SPLIT';
+   
+  }
+
+  // each time i click on the sart/split button i change the classe of both btn 
+  //to change their CSS
+  btnRightElement.classList.toggle('split');
+  btnLeftElement.classList.toggle('stop');
+ 
+ 
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+
+    //check is the chrono is runing, if do a split
+    //if not reset it
+    if(chronometer.intervalId===null && btnLeftElement.classList.contains('start')){
+      chronometer.reset();
+      this.printTime();
+      splitsElement.innerHTML=''
+      
+    }else{
+      
+      const newLi = document.createElement('li');
+      newLi.innerHTML= chronometer.split();
+
+      splitsElement.appendChild(newLi);
+      
+    }
+    
+  
 });
